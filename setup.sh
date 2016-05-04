b4321a4f5caa39b08dfa89b82552b98cee3632e3
Kimbsy <lordkimber@gmail.com>
2016-05-04 22:10:58 +0100

    installing from dotfiles repo

--- setup.sh
+++ setup.sh
@@ -4,23 +4,23 @@
 # Script for installing software and configuring settings to get from a fresh install to just how I like it. #
 ##############################################################################################################
 
-# Ensure script is run with root priveleges
-if [[ $EUID -ne 0 ]]; then
-	printf "You must run this script as root\n" 1>&2
-	exit 1
-fi
-
 # Add repositories.
-add-apt-repository -y ppa:webupd8team/sublime-text-3
+sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
 
 # Update apt.
 sudo apt-get -y update && sudo apt-get -y dist-upgrade && sudo apt-get -y autoremove && sudo apt-get -y autoclean
 
 # Install software.
-apt-get install -y vim tree htop lm-sensors sublime-text-installer vlc nemo
+sudo apt-get install -y vim tree htop lm-sensors sublime-text-installer vlc nemo openjdk-8-jdk
 
-# Clone repos.
+# Clone repos
 git clone git@github.com:Kimbsy/dotfiles.git ~/Projects/dotfiles
+
+# Install functions and aliases from dotfiles repo.
+cd ~/Projects/dotfiles/
+./install.sh ~/.bashrc
+cd
+source ~/.bashrc
 
 # Configure settings.
 gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll true
