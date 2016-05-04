2cae67a759d9c6145794e6ec84721fd75f06b689
Kimbsy <lordkimber@gmail.com>
2016-05-04 21:12:02 +0100

    making sure script is run as root

--- setup.sh
+++ setup.sh
@@ -4,18 +4,23 @@
 # Script for installing software and configuring settings to get from a fresh install to just how I like it. #
 ##############################################################################################################
 
+# Ensure script is run with root priveleges
+if [[ $EUID -ne 0 ]]; then
+	printf "You must run this script as root\n" 1>&2
+	exit 1
+fi
+
 # Add repositories.
 add-apt-repository -y ppa:webupd8team/sublime-text-3
 
 # Update apt.
-sudo apt-get update && sudo apt-get -y dist-upgrade && sudo apt-get autoremove && sudo apt-get autoclean
+sudo apt-get -y update && sudo apt-get -y dist-upgrade && sudo apt-get -y autoremove && sudo apt-get -y autoclean
 
 # Install software.
-apt-get install -y vim tree htop lm-sensors sublime-text-installer
+apt-get install -y vim tree htop lm-sensors sublime-text-installer vlc nemo
 
 # Clone repos.
 git clone git@github.com:Kimbsy/dotfiles.git ~/Projects/dotfiles
 
 # Configure settings.
 gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll true
-
